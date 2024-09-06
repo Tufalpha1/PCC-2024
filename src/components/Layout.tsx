2ab6688949c6f05feffaa5c2cab3aab96aedc526
Tufalpha1 <zaynzafar18@gmail.com>
2024-09-07 00:11:24 +0500

    fix animation and ui

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,7 +3,7 @@ import Footer from "./Footer";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="selection:bg-[#aa0000] selection:text-black overflow-x-hidden !scroll-smooth">
+    <div className="selection:bg-[#aa0000] selection:text-black overflow-hidden !scroll-smooth">
       <Navbar />
       <main>{children}</main>
       <Footer />
