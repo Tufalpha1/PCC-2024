cd026dcfeb47c046b42de8e1dbc0600fe829dec6
Tufalpha1 <zaynzafar18@gmail.com>
2024-09-12 06:23:58 +0500

    change logo height

--- src/components/Organizers.tsx
+++ src/components/Organizers.tsx
@@ -12,48 +12,48 @@ const Organizers = () => {
       id="organizers"
     >
       <div className="px-4 mx-auto max-w-2xl sm:px-6 lg:px-8">
-          <h2 className="font-heading uppercase text-4xl sm:text-5xl xl:text-6xl 2xl:text-7xl text-center mx-auto my-16">
-            Organizers
-          </h2>
-          <div className="grid place-items- grid-cols-1 mt-10 gap-y-6 xl:mt-0 sm:grid-cols-2 sm:gap-y-8">
-            <img
-              className="object-contain w-auto mx-auto h-16 sm:h-24"
-              src={nccs}
-              alt=""
-            />
-            <img
-              className="object-contain w-auto mx-auto h-24 sm:h-28"
-              src={airoverflow}
-              alt=""
-            />
-          </div>
+        <h2 className="font-heading uppercase text-4xl sm:text-5xl xl:text-6xl 2xl:text-7xl text-center mx-auto my-16">
+          Organizers
+        </h2>
+        <div className="grid place-items- grid-cols-1 mt-10 gap-y-6 xl:mt-0 sm:grid-cols-2 sm:gap-y-8">
+          <img
+            className="object-contain w-auto mx-auto h-24 sm:h-28"
+            src={airoverflow}
+            alt="airoverflow-logo"
+          />
+          <img
+            className="object-contain w-auto mx-auto h-16 sm:h-24"
+            src={nccs}
+            alt="nccs-logo"
+          />
+        </div>
       </div>
       <div className="px-4 mx-auto max-w-4xl sm:px-6 lg:px-8 py-12">
-          <h2 className="font-heading uppercase text-4xl sm:text-5xl xl:text-6xl 2xl:text-7xl text-center mx-auto my-16 stroke text-transparent">
-            Patrons
-          </h2>
-          <div className="grid items-center grid-cols-1 mt-10 gap-y-6 xl:mt-0 sm:grid-cols-2 sm:gap-y-8 lg:grid-cols-4 lg:gap-x-8">
-            <img
-              className="object-contain w-auto mx-auto h-20 sm:h-20"
-              src={hec}
-              alt=""
-            />
-            <img
-              className="object-contain w-auto mx-auto h-20 sm:h-20"
-              src={au}
-              alt=""
-            />
-            <img
-              className="object-contain w-auto mx-auto h-20 sm:h-24"
-              src={govt}
-              alt=""
-            />
-            <img
-              className="object-contain w-auto mx-auto h-28 sm:h-28"
-              src={byteboltsec}
-              alt=""
-            />
-          </div>
+        <h2 className="font-heading uppercase text-4xl sm:text-5xl xl:text-6xl 2xl:text-7xl text-center mx-auto my-16 stroke text-transparent">
+          Patrons
+        </h2>
+        <div className="grid items-center grid-cols-1 mt-10 gap-y-6 xl:mt-0 sm:grid-cols-2 sm:gap-y-8 lg:grid-cols-4 lg:gap-x-8">
+          <img
+            className="object-contain w-auto mx-auto h-16 sm:h-20"
+            src={hec}
+            alt="hec-logo"
+          />
+          <img
+            className="object-contain w-auto mx-auto h-16 sm:h-20"
+            src={au}
+            alt="au-logo"
+          />
+          <img
+            className="object-contain w-auto mx-auto h-20 sm:h-24"
+            src={govt}
+            alt="govt-logo"
+          />
+          <img
+            className="object-contain w-auto mx-auto h-28 sm:h-28"
+            src={byteboltsec}
+            alt="byteboltsec-logo"
+          />
+        </div>
       </div>
     </section>
   );
