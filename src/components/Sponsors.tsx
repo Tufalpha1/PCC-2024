d2428cc3669ecb7cea8e3d2be548409f9b3c35d7
Muhammad Zain Ul Abideen <zaynzafar18@gmail.com>
2024-10-04 21:38:09 +0500

    replace sponsor logo

--- src/components/Sponsors.tsx
+++ src/components/Sponsors.tsx
@@ -1,6 +1,6 @@
 import offsec from "../assets/img/sponsors/offsec.jpeg";
 import parrotctf from "../assets/img/sponsors/parrot-ctf.webp";
-import letsdefend from "../assets/img/sponsors/letsdefend.png";
+import letsdefend from "../assets/img/sponsors/lets-defend.png";
 import rootme from "../assets/img/sponsors/rootme.png"
 import futurenostics from "../assets/img/sponsors/futurenostics.png"
 
@@ -16,56 +16,56 @@ const Sponsors = () => {
       </h1>
       <div className="relative my-24 w-full overflow-hidden bg-black before:absolute before:left-0 before:top-0 before:z-[2] before:h-full before:w-[100px] before:bg-[linear-gradient(to_right,black_0%,rgba(255,255,255,0)_100%)] before:content-[''] after:absolute after:right-0 after:top-0 after:z-[2] after:h-full after:w-[100px] after:-scale-x-100 after:bg-[linear-gradient(to_right,black_0%,rgba(255,255,255,0)_100%)] after:content-['']">
         <div className="animate-infinite-slider flex w-[calc(250px*10)]">
-          <div className="flex w-full items-center justify-center sm:gap-[50px]">
+          <div className="flex w-full items-center justify-center">
             <img
-              className="object-contain w-auto mx-auto h-16 sm:h-20"
+              className="object-contain w-auto mx-auto h-16"
               src={offsec}
               alt="offsec-logo"
             />
             <img
-              className="object-contain w-auto mx-auto h-24 sm:h-32"
+              className="object-contain w-auto mx-auto h-24"
               src={parrotctf}
               alt="parrotctf-logo"
             />
             <img
-              className="object-contain w-auto mx-auto h-16 sm:h-20"
+              className="object-contain w-auto mx-auto h-12"
               src={letsdefend}
               alt="letsdefend-logo"
             />
             <img
-              className="object-contain w-auto mx-auto h-12 sm:h-16"
+              className="object-contain w-auto mx-auto h-12"
               src={rootme}
               alt="rootme-logo"
             />
             <img
-              className="object-contain w-auto mx-auto h-24 sm:h-32"
+              className="object-contain w-auto mx-auto h-24"
               src={futurenostics}
               alt="futurenostics-logo"
             />
           </div>
-          <div className="flex w-full items-center justify-center sm:gap-[50px]">
+          <div className="flex w-full items-center justify-center">
           <img
-              className="object-contain w-auto mx-auto h-16 sm:h-20"
+              className="object-contain w-auto mx-auto h-16"
               src={offsec}
               alt="offsec-logo"
             />
             <img
-              className="object-contain w-auto mx-auto h-24 sm:h-32"
+              className="object-contain w-auto mx-auto h-24"
               src={parrotctf}
               alt="parrotctf-logo"
             />
             <img
-              className="object-contain w-auto mx-auto h-16 sm:h-20"
+              className="object-contain w-auto mx-auto h-12"
               src={letsdefend}
               alt="letsdefend-logo"
             />
             <img
-              className="object-contain w-auto mx-auto h-12 sm:h-16"
+              className="object-contain w-auto mx-auto h-12"
               src={rootme}
               alt="rootme-logo"
             />
             <img
-              className="object-contain w-auto mx-auto h-24 sm:h-32"
+              className="object-contain w-auto mx-auto h-24"
               src={futurenostics}
               alt="futurenostics-logo"
             />
