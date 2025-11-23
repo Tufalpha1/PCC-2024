f59e81720ff0ce1ba28bb97f06efd0006a0d3c62
Muhammad Zain Ul Abideen <zaynzafar18@gmail.com>
2025-11-24 01:33:22 +0500

    minor touching

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,6 @@ import Layout from "./components/Layout";
 import Organizers from "./components/Organizers";
 import Sponsors from "./components/Sponsors";
 import Stats from "./components/Stats";
-import Warzone from "./components/Warzone";
 import Register from "./components/Register";
 
 function App() {
@@ -38,8 +37,7 @@ function App() {
               {/* <Scoreboard />
               <Podium /> */}
               <Showdown />
-              <SectionDivider className="bg-gradient-to-r from-[#ff0000] to-[#aa0000] text-white" />
-              <Warzone />
+              <SectionDivider className="bg-gradient-to-r from-[#ff0000] to-[#aa0000] text-white" />    
               <SectionDivider className="bg-gradient-to-r from-[#45cc2d] to-[#092004] text-white" />
               <Categories />
               <SectionDivider className="bg-black text-white" />
